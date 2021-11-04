b98909270879d1cd8ce27b44d9aaa072832b1dc7
fazalfarhan01 <fazal.farhan@gmail.com>
2021-11-04 13:59:13 +0530

    remove arm64 support

--- app/src/setup.sh
+++ app/src/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt update && apt install wget curl net-tools -y
+apt update && apt install wget curl net-tools libatomic1 -y
 archs=`uname -m`
 if [ $archs = "x86_64" ]; then
     file=bin_64
@@ -9,6 +9,6 @@ else
 fi
 echo "Fetching $file"
 mkdir /download
-wget -c https://brightdata.com/static/earnapp/$file -O /download/earnapp
+wget -c --no-check-certificate https://brightdata.com/static/earnapp/$file -O /download/earnapp
 echo | md5sum /download/earnapp
 chmod +x /download/earnapp
