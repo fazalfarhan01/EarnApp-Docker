84454adf431f88461a8f78967034027d9b7c84f1
fazalfarhan01 <fazal.farhan@gmail.com>
2021-11-20 09:23:01 +0530

    tested on arm64/amd64

--- app/src/setup.sh
+++ app/src/setup.sh
@@ -1,14 +1,36 @@
 #!/bin/bash
 
-apt update && apt install wget curl net-tools libatomic1 -y
+spinner(){
+    PID=$!
+    i=1
+    sp="/-\|"
+    echo -n "$1 "
+    while [ -d /proc/$PID ]
+    do
+        printf "\b${sp:i++%${#sp}:1}"
+        sleep 0.3s
+    done
+    printf "\b✓\n"
+}
+
 archs=`uname -m`
 if [ $archs = "x86_64" ]; then
     file=bin_64
+elif [ $archs = "amd64" ]; then
+    file=bin_64
+elif [ $archs = "armv7l" ]; then
+    file=armv7
+elif [ $archs = "armv6l" ]; then
+    file=armv7
+elif [ $archs = "aarch64" ]; then
+    file=aarch64
+elif [ $archs = "arm64" ]; then
+    file=aarch64
 else
     file=armv7
 fi
-echo "Fetching $file"
 mkdir /download
-wget -cq --no-check-certificate https://brightdata.com/static/earnapp/$file -O /download/earnapp
+wget -cq --no-check-certificate https://brightdata.com/static/earnapp/$file -O /download/earnapp > /dev/null &
+spinner "Downloading $file"
 echo | md5sum /download/earnapp
 chmod +x /download/earnapp
